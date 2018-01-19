31471bb7d012c81f2a5523e0d0e78bc7c3dbcdbd
pllearns <pllearns@gmail.com>
2018-01-19 01:17:53 -0800

    Update compiler and test initial contract state and update

--- compile.js
+++ compile.js
@@ -5,6 +5,5 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf-8');
 
-module exports = solc.compile(source, 1).contracts[:Inbox];
-
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
 
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,17 +1,38 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = require('web3'); // This is a constructor function!
+const Web3 = require('web3');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile')
 
-beforeEach(() => {
-  web3.eth.getAccounts().then(fetchedAccounts => {
-    console.log(fetchedAccounts);
-  });
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hello!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+  
+  inbox.setProvider(provider);
 });
 
 describe('contract', () => {
   it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hello!');
+  })
+
+  it('sets a message', async () => {
+    await inbox.methods.setMessage('Later!').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Later!');
   });
 });
 
